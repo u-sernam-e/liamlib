5251bd8fdaf8d78b17447a71bbcc1a16b1cee7c0
u-sernam-e <90429445+u-sernam-e@users.noreply.github.com>
2022-11-07 18:44:42 -0500

    Add files via upload

--- headers/fontstorage.h
+++ headers/fontstorage.h
@@ -7,24 +7,14 @@
 
 class FontStorage
 {
-public:
-    enum FontNames {
-        KINGTHINGSEXETER_T,
-        KINGTHINGSFACETYPE_T,
-        MAXFONTNAMES_T
-    };
 private:
-    std::array<std::string, MAXFONTNAMES_T> m_fontStrs{
-        "fonts/KingthingsExeter.ttf",
-        "fonts/KingthingsFacetype.ttf"
-    };
-    std::array<Font, MAXFONTNAMES_T> m_fonts;
+    std::vector<std::pair<Font, std::string>> m_fonts;
 public:
-    void init() // call this AFTER you call InitWindow()
+    void init(const std::vector<std::string>& fontStrs) // call this AFTER you call InitWindow()
     {
-        for (int i{}; i < MAXFONTNAMES_T; ++i)
+        for (int i{}; i < fontStrs.size(); ++i)
         {
-            m_fonts[i] = LoadFontEx(m_fontStrs[i].c_str(), 64, 0, 250);
+            m_fonts.push_back({LoadFontEx(fontStrs[i].c_str(), 64, 0, 250), fontStrs[i]});
         }
     }
 
@@ -32,14 +22,20 @@ public:
     {
         for (auto& f : m_fonts)
         {
-            UnloadFont(f);
+            UnloadFont(f.first);
         }
     }
 
-    const Font& get(FontNames f) const { return m_fonts[f]; }
+    const Font& get(std::string s) const
+    {
+        for (auto& t : m_fonts)
+        {
+            if (t.second == s)
+                return t.first;
+        }
+        return m_fonts[0].first;
+    }
 };
-
-using FontNam = FontStorage::FontNames;
 
 FontStorage& fontStrg(); // im a sneaky snitch, i use a function instead of a pure global variable to get past c++ being stupid asf
                             // also you probably shouldn't make a local FontStorage, i think it would just waste memory
